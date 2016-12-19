5c63806c9b143dc1a0fc1475e2eaec0b52b839c2
Tamar <you@example.com>
2016-12-19 15:25:34 +0200

    Added import AST.* to parser, added consts to ICTypeInfo.

--- WINDOWS_COMMAND_LINE/FOLDER_3_src/SemanticAnalysis/ICTypeInfo.java
+++ WINDOWS_COMMAND_LINE/FOLDER_3_src/SemanticAnalysis/ICTypeInfo.java
@@ -5,6 +5,10 @@ public class ICTypeInfo {
 	public String ICType; //"int" for int and "string" for string
 	public int pointerDepth; // 0 for regular one.
 	
+	public static final String IC_TYPE_INT = "int";
+	public static final String IC_TYPE_STRING = "string";
+	public static final String IC_TYPE_VOID = "void";
+	
 	// usage: for validate function, when we want to say "everything is ok" but we don't have certain ICTypeInfo to return.
 	public ICTypeInfo()
 	{
