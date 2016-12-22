137396301fc382a2212022532a911a1f7f68ab48
Tamar <you@example.com>
2016-12-22 15:54:07 +0200

    Implemented AST_STMT_CALL.validate

--- WINDOWS_COMMAND_LINE/FOLDER_3_src/AST/AST_STMT_CALL.java
+++ WINDOWS_COMMAND_LINE/FOLDER_3_src/AST/AST_STMT_CALL.java
@@ -1,5 +1,10 @@
 package AST;
 
+import SemanticAnalysis.ICTypeInfo;
+import SemanticAnalysis.NullFieldException;
+import SemanticAnalysis.SemanticAnalysisException;
+import Utils.DebugPrint;
+
 public class AST_STMT_CALL extends AST_STMT 
 {
 	public AST_CALL call;
@@ -7,5 +12,31 @@ public class AST_STMT_CALL extends AST_STMT
 	public AST_STMT_CALL(AST_CALL call)
 	{
 		this.call = call;
+	}
+	
+	/**
+	 * @brief 	Validates the call-statement by validating the call.
+	 * 
+	 * @param	className - the name of the IC class which is currently being analyzed.
+	 * 
+	 * @return 	an empty ICTypeInfo if the call-statement is valid, null otherwise.
+	 */
+	@Override
+	public ICTypeInfo validate(String className) throws SemanticAnalysisException
+	{
+		if (call == null)
+		{
+			throw new NullFieldException("AST_STMT_CALL.call");
+		}
+		
+		// Validating the call
+		if (call.validate(className) == null)
+		{
+			DebugPrint.print("AST_STMT_CALL.validate: The call isn't valid.");
+			return null;
+		}
+		
+		// The call-statement is valid
+		return new ICTypeInfo();
 	}
 }
