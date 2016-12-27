80c11535552709bc33141751b8868242f540fadd
Tamar <you@example.com>
2016-12-27 20:31:22 +0200

    Changed the Ctor so the list will stay in the given order

--- WINDOWS_COMMAND_LINE/FOLDER_3_src/AST/AST_FORMALS_LIST.java
+++ WINDOWS_COMMAND_LINE/FOLDER_3_src/AST/AST_FORMALS_LIST.java
@@ -5,17 +5,19 @@ import SemanticAnalysis.ICTypeInfo;
 import SemanticAnalysis.SemanticAnalysisException;
 import SemanticAnalysis.SymbolTable;
 import SemanticAnalysis.VariableSymbolInfo;
+import Utils.DebugPrint;
 
 public class AST_FORMALS_LIST extends AST_Node
 {
 	public String functionName;
-	public AST_TYPE formal_type;
-	public String formal_name;
+	public AST_TYPE formal_type; // shouldn't be null
+	public String formal_name; // shouldn't be null
 	public AST_FORMALS_LIST tail;
 	
 	public AST_FORMALS_LIST(AST_TYPE formal_type,String formal_name,AST_FORMALS_LIST tail)
 	{
 		this.functionName=null;
+		/*
 		AST_FORMALS_LIST iterator=tail;
 		if((iterator!=null)&&(!iterator.isEmpty()))
 		{
@@ -35,10 +37,7 @@ public class AST_FORMALS_LIST extends AST_Node
 			this.formal_type=formal_type;
 			this.tail=null;
 		}
-		
-		
-		
-		
+		*/
 		this.tail=tail;
 		this.formal_type=formal_type;
 		this.formal_name=formal_name;
@@ -52,21 +51,41 @@ public class AST_FORMALS_LIST extends AST_Node
 		}
 		else
 		{
+			// Validating the head
 			ICTypeInfo formalICType=this.formal_type.validate(className);
-			// iterate over the list
-			if(formalICType!=null)
+			if (formalICType == null)
 			{
-				if(SymbolTable.doesSymbolExistInCurrentScope(formal_name))
+				String debugMessage = String.format("AST_FORMALS_LIST.validate: The formal '%s' of the method '%s.%s' has an invalid type.",
+						formal_name, className, functionName);
+				DebugPrint.print(debugMessage);
+				return null;
+			}
+			if(SymbolTable.doesSymbolExistInCurrentScope(formal_name))
+			{
+				String debugMessage = String.format("AST_FORMALS_LIST.validate: The formal '%s' of the method '%s.%s' is redefined.",
+						formal_name, className, functionName);
+				DebugPrint.print(debugMessage);
+				return null;
+			}
+			if(SymbolTable.addFormalToMethod(className,functionName, new VariableSymbolInfo(formal_name, formalICType))==false)
+			{
+				String debugMessage = String.format("AST_FORMALS_LIST.validate: Failed adding the formal '%s' to the method '%s.%s'.",
+						formal_name, className, functionName);
+				DebugPrint.print(debugMessage);
+				return null;
+			}
+			
+			SymbolTable.insertNewSymbol(new VariableSymbolInfo(formal_name, formalICType));
+				
+			// Validating the rest of the list
+			if (this.tail != null)
+			{
+				tail.functionName = this.functionName;
+				if (tail.validate(className) == null)
 				{
+					DebugPrint.print("AST_FORMALS_LIST.validate: The tail isn't valid.");
 					return null;
 				}
-				if(SymbolTable.addFormalToMethod(className,functionName, new VariableSymbolInfo(formal_name, formalICType))==false)
-				{
-					return null;
-				}
-				
-				SymbolTable.insertNewSymbol(new VariableSymbolInfo(formal_name, formalICType));
-				
 			}
 		}
 		return new ICTypeInfo();
