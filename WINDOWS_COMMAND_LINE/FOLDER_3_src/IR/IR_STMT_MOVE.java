bbdb7812b3ae4de9b8883664faa85c9d175403fc
Tamar <you@example.com>
2017-01-13 09:04:49 +0200

    Made IR_STMT_MOVE receive a generic IR_EXP in its C'tor, instead of IR_EXP_MEM

--- WINDOWS_COMMAND_LINE/FOLDER_3_src/IR/IR_STMT_MOVE.java
+++ WINDOWS_COMMAND_LINE/FOLDER_3_src/IR/IR_STMT_MOVE.java
@@ -3,11 +3,11 @@ package IR;
 public class IR_STMT_MOVE extends IR_STMT 
 {
 	// fields
-	public IR_EXP dst;
+	public IR_EXP dst; // can be either an address (not IR_EXP_MEM!) or a temp.
 	public IR_EXP src;
 	
 	// C'tor
-	public IR_STMT_MOVE(IR_EXP_MEM dst, IR_EXP src)
+	public IR_STMT_MOVE(IR_EXP dst, IR_EXP src)
 	{
 		this.dst = dst;
 		this.src = src;
