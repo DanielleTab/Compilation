cddb7b053daf545377278ea55ebdb66c20d24f3e
Tamar <you@example.com>
2017-02-04 17:28:21 +0200

    Added a TODO in AST_LOCATION_SUBSCRIPT

--- WINDOWS_COMMAND_LINE/FOLDER_3_src/AST/AST_LOCATION_SUBSCRIPT.java
+++ WINDOWS_COMMAND_LINE/FOLDER_3_src/AST/AST_LOCATION_SUBSCRIPT.java
@@ -1,8 +1,10 @@
 package AST;
 
 import IR.BinOperation;
+import IR.IR_EXP;
 import IR.IR_EXP_BINOP;
 import IR.IR_EXP_MEM;
+import IR.IR_LITERAL_CONST;
 import SemanticAnalysis.ClassIsNotInSymbolTableException;
 import SemanticAnalysis.ClassOrFunctionNamesNotInitializedExecption;
 import SemanticAnalysis.ICTypeInfo;
@@ -58,6 +60,14 @@ public class AST_LOCATION_SUBSCRIPT extends AST_LOCATION
 		this.var.currentFunctionName=this.currentFunctionName;
 		this.subscript.currentClassName=this.currentClassName;
 		this.subscript.currentFunctionName=this.currentFunctionName;
+		
+		// TODO: Add here a check of the array size, and then use the commented code.
+		/*
+		IR_EXP subscriptIR = subscript.createIR();
+		IR_LITERAL_CONST shiftSize = new IR_LITERAL_CONST(1);
+		IR_EXP_BINOP shiftedSubscript = new IR_EXP_BINOP(subscriptIR, shiftSize, BinOperation.PLUS);
+		return new IR_EXP_BINOP(this.var.createIR(), shiftedSubscript, BinOperation.PLUS);
+		*/
 		
 		return new IR_EXP_BINOP(this.var.createIR(),this.subscript.createIR(),BinOperation.PLUS);
 	}
