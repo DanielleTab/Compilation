397fdba8be8064d2b5035e30494f411dbf61dd3a
Tamar <you@example.com>
2017-02-06 16:01:04 +0200

    Implemented IR_STMT_CALL.generateCode

--- WINDOWS_COMMAND_LINE/FOLDER_3_src/IR/IR_STMT_CALL.java
+++ WINDOWS_COMMAND_LINE/FOLDER_3_src/IR/IR_STMT_CALL.java
@@ -1,9 +1,23 @@
 package IR;
 
-public class IR_STMT_CALL extends IR_STMT{
+import java.io.IOException;
+
+public class IR_STMT_CALL extends IR_STMT
+{
 	public IR_CALL call;
+	
 	public IR_STMT_CALL(IR_CALL call)
 	{
 		this.call=call;
+	}
+	
+	/**
+	 * @brief	Generates code for the statement call by generating code
+	 * 			for the call itself.
+	 */
+	@Override
+	public void generateCode() throws IOException 
+	{
+		call.generateCode();
 	}
 }
