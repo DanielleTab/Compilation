077e8d1aff4b466229c0b9cd35356dfc96a2f0b9
dhjkd <dhjkd>
2017-02-10 15:22:28 +0200

    add array initialization

--- WINDOWS_COMMAND_LINE/FOLDER_3_src/IR/IR_EXP_NEW_ARRAY.java
+++ WINDOWS_COMMAND_LINE/FOLDER_3_src/IR/IR_EXP_NEW_ARRAY.java
@@ -36,36 +36,48 @@ public class IR_EXP_NEW_ARRAY extends IR_EXP{
 			 * sw zeroTemp,tempIterator
 			 * addi tempIterator,tempIterator,1
 			 * addi tempIndex,tempIndex,1
+			 * goto Label_num_loop
 			 * Label_num_endLoop
 			 */
-			/*
-			StringNLBuilder printed = new StringNLBuilder();
-			CodeGen_Temp temp = TempGenerator.getAndAddNewTemp();
+			
+			CodeGen_Temp currIndex = TempGenerator.getAndAddNewTemp();
+			CodeGen_Temp tempIterator = TempGenerator.getAndAddNewTemp();
 			CodeGen_Temp zeroTemp = TempGenerator.getAndAddNewTemp();
+			String initArrayLabel = String.format("Label_%d_init_array", AssemblyFilePrinter.addLabelIndex());
+			String startLoopLabel = String.format("Label_%d_start_loop", AssemblyFilePrinter.addLabelIndex());
+			String endLoopLabel =  String.format("Label_%d_end_loop", AssemblyFilePrinter.addLabelIndex());
+			printed.appendNL(initArrayLabel+":");
 			printed.appendNL(String.format("li %s,0", zeroTemp.getName()));
-			printed.appendNL(String.format("mov %s,%s", temp.getName(), addressOnHeap.getName()));
-			for(int i=0;i<classInfo.fields.size();i++)
-			{
-				printed.appendNL(String.format("addi %s,%s,%d", temp.getName(), temp.getName(),SymbolTable.ADDRESS_SIZE));
-				printed.appendNL(String.format("sw %s,0(%s)", zeroTemp.getName(),temp.getName()));
-			}
-			AssemblyFilePrinter.getInstance(null).write(printed.toString());*/
+			printed.appendNL(String.format("li %s,1", currIndex.getName()));
+			printed.appendNL(String.format("mov %s,%s", tempIterator.getName(),addressOnHeap.getName()));
+			printed.appendNL(String.format("addi %s,%s,%d", tempIterator.getName(),tempIterator.getName(),SymbolTable.ADDRESS_SIZE));
+			printed.appendNL(startLoopLabel+":");
+			printed.appendNL(String.format("bgt %s,%s,%s",currIndex.getName(),arraySize.getName(),endLoopLabel));
+			printed.appendNL(String.format("sw %s,0(%s)", zeroTemp.getName(),tempIterator.getName()));
+			printed.appendNL(String.format("addi %s,%s,%d", tempIterator.getName(),tempIterator.getName(),SymbolTable.ADDRESS_SIZE));
+			printed.appendNL(String.format("addi %s,%s,1", currIndex.getName(), currIndex.getName()));
+			printed.appendNL(String.format("j %s", startLoopLabel));
+			printed.appendNL(endLoopLabel+":");
+			
 		}
 		
 	
 	public CodeGen_Temp generateCode() throws IOException
 	{
-		CodeGen_Temp generatedSize = this.size.generateCode();
-		CodeGen_Temp generatedSizePlusOne = TempGenerator.getAndAddNewTemp();
+		CodeGen_Temp generatedNumOfElements = this.size.generateCode();
+		CodeGen_Temp generatedNumOfElementsPlusOne = TempGenerator.getAndAddNewTemp();
 		StringNLBuilder printed = new StringNLBuilder();
-		
+		CodeGen_Temp fourTemp = TempGenerator.getAndAddNewTemp();
+		printed.appendNL(String.format("li %s,4",fourTemp.getName()));
 		// add one to the generatedSize because we want to allocate (size+1) cells on the heap -
 		// the first cell for the size.
-		printed.appendNL(String.format("addi %s,%s,1", generatedSizePlusOne.getName(), generatedSize.getName()));
-		CodeGen_Temp heapAddress = CodeGen_Utils.codeGen_malloc(printed,generatedSizePlusOne);
+		printed.appendNL(String.format("addi %s,%s,1", generatedNumOfElementsPlusOne.getName(), generatedNumOfElements.getName()));
+		CodeGen_Temp generatedSize = TempGenerator.getAndAddNewTemp();
+		printed.appendNL(String.format("mul %s,%s,%s", generatedSize.getName(),generatedNumOfElementsPlusOne.getName(),fourTemp.getName()));
+		CodeGen_Temp heapAddress = CodeGen_Utils.codeGen_malloc(printed,generatedSize);
 		// put the first element in the array to be the array size.
-		printed.appendNL(String.format("sw %s,0(%s)",generatedSize.getName(),heapAddress.getName()));
-		generateNullForArrayElements(heapAddress,generatedSize,printed);
+		printed.appendNL(String.format("sw %s,0(%s)",generatedNumOfElements.getName(),heapAddress.getName()));
+		generateNullForArrayElements(heapAddress,generatedNumOfElements,printed);
 		AssemblyFilePrinter.getInstance(null).write(printed.toString());
 		return heapAddress;
 	}
