904ee2ecafc9b4086f588f75bdbabed96f8583a0
Tamar <you@example.com>
2017-02-28 16:16:42 +0200

    Added binop test

--- WINDOWS_COMMAND_LINE/FOLDER_10_tester/FinalTester.java
+++ WINDOWS_COMMAND_LINE/FOLDER_10_tester/FinalTester.java
@@ -166,7 +166,7 @@ public class FinalTester
 		outputWriter = new PrintStream(new FileOutputStream(TESTER_OUTPUT_FILE_NAME));
 		//System.setOut(outputWriter);
 		
-		runSpecificTest("scoping_all", TesterMode.AUTO_CLOSE);
+		runSpecificTest("binops", TesterMode.AUTO_CLOSE);
 	    //runAllTests(TesterMode.AUTO_CLOSE_MINIMIZE_CMD);
 	}
 	
